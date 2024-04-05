a4cf33e105c2b90b34766312dacf13ecd3bd6391
ossian <ossian.edstrom@live.se>
2024-04-06 00:52:14 +0200

    Whole project

---
+++ Assets/Scritps/TagCollection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagCollection : MonoBehaviour
+{
+    Dictionary<string, Color> TagDict = new Dictionary<string, Color>()
+    {
+        {"Helpful", Color.green},
+    };
+
+}
